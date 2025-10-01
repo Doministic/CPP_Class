6ae49d5623ab31da80c4b96421726dc8780f07b4
Dominic <dominic.lagle444@gmail.com>
2025-10-01 12:06:37 -0700

    Adding future class information

--- 2. Pointers, References, Arrays, Enums/Week_9/Class 17/class17_guessingGame.cpp
+++ 2. Pointers, References, Arrays, Enums/Week_9/Class 17/class17_guessingGame.cpp
@@ -13,10 +13,28 @@ enum difficulty
 	Master = 4
 };
 
+// forward declaration:
+static difficulty setDifficultyFromUserInput(int& userInput);
+
 std::random_device rd;  // Obtain a random number from hardware
 std::mt19937 gen(rd()); // Seed the generator
 
-static std::pair<int, int> setDifficulty(difficulty level)
+static void printMainMenu()
+{
+	std::cout << "-----MAIN MENU-----\n";
+	std::cout << "1. Start Game\n";
+	std::cout << "2. Set Difficulty\n";
+	std::cout << "3. Exit\n";
+}
+
+static int getRandomNumberInRange(signed int& min, signed int& max)
+{
+	std::uniform_int_distribution<> dist(min, max); // Define the range
+	return dist(gen);
+}
+
+
+static std::pair<std::pair<int, int>, std::pair<int, int>> setDifficulty(difficulty level)
 {
 	signed int min = 1, max = 100;
 	unsigned int attempts = 5;
@@ -46,7 +64,7 @@ static std::pair<int, int> setDifficulty(difficulty level)
 	case Master:
 		min = -10000;
 		max = 100000;
-		attempts = 50;
+		attempts = 30;
 		break;
 	default:
 		min = 1;
@@ -55,51 +73,11 @@ static std::pair<int, int> setDifficulty(difficulty level)
 		break;
 	}
 	signed int randomNumber = getRandomNumberInRange(min, max);
-	return {randomNumber, attempts};
+	return { {min, max}, { randomNumber, attempts } };
 }
 
-static int getRandomNumberInRange(signed int& min, signed int& max)
+static int printDifficulty(difficulty diffLevel)
 {
-	std::uniform_int_distribution<> dist(min, max); // Define the range
-	return dist(gen);
-}
-
-static difficulty setDifficultyFromUserInput(int& userInput)
-{
-
-	difficulty level = static_cast<difficulty>(userInput);
-	if (userInput < 0 || userInput > 4)
-	{
-		std::cout << "Invalid input. Defaulting to Normal.\n";
-		level = Normal;
-	}
-	else
-	{
-		printDifficulty(userInput);
-		level = static_cast<difficulty>(userInput);
-	}
-
-	return level;
-}
-
-static void printDifficultyOptions()
-{
-	std::cout << "Please select a difficulty level (0-4): ";
-	std::cout << "Difficulty Levels:\n";
-	std::cout << "-----------------------------------------------------------------------------------\n";
-	std::cout << " To select a difficulty enter the number corresponding to the difficulty level.\n";
-	std::cout << "-----------------------------------------------------------------------------------\n";
-	std::cout << "0. Easy: Min set to 1 - Max set to 100 - Number of attempts set to 10\n";
-	std::cout << "1. Normal: min set to 1 - Max set to 100 - Number of attempts set to 5\n";
-	std::cout << "2. Hard: Min set to 1 - Max set to 5000 - Number of attempts set to 15\n";
-	std::cout << "3. Expert: Min set to -1000 - Max set to 10000 - Number of attempts set to 30\n";
-	std::cout << "4. Master: Min set to -10000 - Max set to 100000 - Number of attempts set to 50\n";
-}
-
-static int printDifficulty(int& level)
-{
-	difficulty diffLevel = static_cast<difficulty>(setDifficultyFromUserInput(level));
-
 	switch (diffLevel)
 	{
 	case Easy:
@@ -124,12 +102,36 @@ static int printDifficulty(int& level)
 	return 0;
 }
 
-static void printMainMenu()
+static difficulty setDifficultyFromUserInput(int& userInput)
 {
-	std::cout << "-----MAIN MENU-----\n";
-	std::cout << "1. Start Game\n";
-	std::cout << "2. Set Difficulty\n";
-	std::cout << "3. Exit\n";
+	std::cout << "User Input: " << userInput << "\n";
+	difficulty level = static_cast<difficulty>(userInput);
+	if (userInput < 0 || userInput > 4)
+	{
+		std::cout << "Invalid input. Defaulting to Normal.\n";
+		level = Normal;
+	}
+	else
+	{
+		level = static_cast<difficulty>(userInput);
+		printDifficulty(level);
+	}
+
+	return level;
+}
+
+static void printDifficultyOptions()
+{
+	std::cout << "Please select a difficulty level (0-4): ";
+	std::cout << "Difficulty Levels:\n";
+	std::cout << "-----------------------------------------------------------------------------------\n";
+	std::cout << " To select a difficulty enter the number corresponding to the difficulty level.\n";
+	std::cout << "-----------------------------------------------------------------------------------\n";
+	std::cout << "0. Easy: Min set to 1 - Max set to 100 - Number of attempts set to 10\n";
+	std::cout << "1. Normal: min set to 1 - Max set to 100 - Number of attempts set to 5\n";
+	std::cout << "2. Hard: Min set to 1 - Max set to 5000 - Number of attempts set to 15\n";
+	std::cout << "3. Expert: Min set to -1000 - Max set to 10000 - Number of attempts set to 30\n";
+	std::cout << "4. Master: Min set to -10000 - Max set to 100000 - Number of attempts set to 50\n";
 }
 
 static int& getUserInput(int& userInput)
@@ -139,16 +141,34 @@ static int& getUserInput(int& userInput)
 	return userInput;
 }
 
+static int playAgain(char *playAgain)
+{
+	std::cout << "Would you like to play again? ('y' or 'n'): ";
+	if (*playAgain == 'y')
+	{
+		std::cout << "Great! Let's play again!\n";
+		return 1;
+	}
+	else
+	{
+		std::cout << "Thank you for playing! Goodbye!\n";
+		return 0;
+	}
+}
+
 int main()
 {
+	int playing = 1;
+	char play = 'y';
+	int numberToGuess;
+	difficulty level = Normal;
+	std::pair<std::pair<int, int>, std::pair<int, int>> difficultyLevel = setDifficulty(Normal); // Default difficulty level
+	int userMainMenuInput;
+	while (playing == 1)
 	{
-		while (true)
-			std::cout << "Welcome to the Guessing Game!\n";
+		std::cout << "Welcome to the Guessing Game!\n";
 		printMainMenu();
-		int numberToGuess;
-		difficulty level = Normal;
-		std::pair<int, int> difficultyLevel = setDifficulty(Normal); // Default difficulty level
-		int userMainMenuInput;
+
 		getUserInput(userMainMenuInput);
 
 		if (userMainMenuInput == 1)
@@ -174,16 +194,40 @@ int main()
 			return -1;
 		}
 
-		int numberToGuess = setDifficulty(level).first;
-		int attempts = setDifficulty(level).second;
+		numberToGuess = setDifficulty(level).second.first;
+		int attempts = setDifficulty(level).second.second;
 
 		std::cout << "The number to guess is set. You have " << attempts << " attempts to guess it.\n";
 
 		int userGuess;
-		//TODO: FINISH the guessing game. 
+		while (attempts > 0)
+		{
+			std::cout << "You have " << attempts << " attempts left. Please enter your guess: ";
+			std::cin >> userGuess;
+			if (userGuess < numberToGuess)
+			{
+				std::cout << "Your guess is too low. Try again. \n";
+			}
+			else if (userGuess > numberToGuess)
+			{
+				std::cout << "Your guess is too high. Try again. \n";
+			}
+			attempts--;
+			if (userGuess == numberToGuess)
+			{
+				std::cout << "Congrats you guess the number: " << numberToGuess << "!\n";
+				std::cout << "You win the game!\n";
+				std::cin >> play;
+				playing = playAgain(&play);
+				break;
+			} else if (attempts == 0)
+			{
+				std::cout << "You have run out of attempts. The number was: " << numberToGuess << ".\n";
+				std::cout << "Better luck next time!\n";
+				std::cin >> play;
+				playing = playAgain(&play);
+			}
+		}
 	}
-
 	return 0;
 }
-
-
